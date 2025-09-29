7e523dbef77a1b5319e36bfdba9d37cdee26823b
Doni Zefironi <86412426+7efirochka@users.noreply.github.com>
2025-09-29 12:32:59 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def com_cd(self, target):
-    new_path = self.current_directory / target
-    self.current_directory = new_path.resolve()
-
-    if not self.current_directory.exists():
-        return f"No such directory: {target}"
-
-    return self.current_directory
